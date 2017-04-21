37d58979de6b707d6ba2c9974db0169b0c39004a
Vladimir Pavlovski <antivoland@gmail.com>
2017-04-21 14:54:29 +0300

    define public interface

---
+++ src/main/java/ru/creditnet/progressbar/ConsoleProgressBar.java
@@ -0,0 +1,14 @@
+package ru.creditnet.progressbar;
+
+/**
+ * @author antivoland
+ */
+public class ConsoleProgressBar {
+    public void step() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public void stepBy(long delta) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+}
